41a1cba7b22d13982e2ce2c8207a2ea04f261092
bommenamadhavi <31123170+bommenamadhavi@users.noreply.github.com>
2017-09-21 14:59:22 +0530

    Update sample.sql

--- sample.sql
+++ sample.sql
@@ -3,9 +3,9 @@
 Drop table if exists `Sample`;
 Delimiter $$
 CREATE TABLE IF NOT EXISTS `Sample` (
-  `Id` int(10) NOT NULL COMMENT 'Name of the Event that is beignn executed',
-  `name` varchar(250) NOT NULL COMMENT 'Value of input parameter to the Stored Procedures if required',
-  `age' int(10) NOT NULL COMMENT 'Name of the Stored Procedure Parameter'
+  `Id` int(10) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `age` int(10) NOT NULL'
 ) ;
 $$
 Delimiter ;
