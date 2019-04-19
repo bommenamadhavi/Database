abb18f15d3237ff6e16839328cd76d75c4464f67
bommenamadhavi <bommena.madhavi@vonkdoth.com>
2019-04-19 15:45:19 +0530

    kk

--- sample.sql
+++ sample.sql
@@ -2,10 +2,10 @@
 /* Table structure for table `Sample Table`*/
 Drop table if exists `Sample`;
 Delimiter $$
-CREATE TABLE IF NOT EXISTS `SampleABC` (
+CREATE TABLE IF NOT EXISTS `Sample` (
   `Id` int(10) NOT NULL,
   `name` varchar(250) NOT NULL,
   `age` int(10) NOT NULL
 ) ;
 $$
-Delimiter;
+Delimiter ;
